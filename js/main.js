8c18a30b762fb18aef22805cf07f4757c78b4066
Shimamura <m_shimamura@tss-hr420-dm22.fc.ca>
2019-01-14 16:18:05 -0500

    done with in class stuff

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(() => {
+	// self-invoking anonymous function (or Lambda)
+	// 
+	// var => means create a variable
+	// whatever comes after te = is waht the variable
+	var images = document.querySelectorAll('img');
+
+	// document is always the webpage 
+	// after the dot is always a method or a property
+
+	console.log(images);
+
+})();
